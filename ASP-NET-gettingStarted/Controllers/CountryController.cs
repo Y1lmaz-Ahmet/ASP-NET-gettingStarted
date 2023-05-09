1219e7844b9c2062f34abcae81f99930f206d44a
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-05-09 12:54:32 +0200

    program.cs aanpassen door dependency injection

--- ASP-NET-gettingStarted/Controllers/CountryController.cs
+++ ASP-NET-gettingStarted/Controllers/CountryController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ASP_NET_gettingStarted.Model;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASP_NET_gettingStarted.Controllers
@@ -7,6 +8,14 @@ namespace ASP_NET_gettingStarted.Controllers
     [ApiController]
     public class CountryController : ControllerBase
     {
+        //Om de repository te kunnen gebruiken in de controller voegen we een variabele van het
+        //type ICountryRepository toe aan de klasse CountryController
+        //en initialiseren deze in de constructor.
+        private ICountryRepository _countryRepository;
 
+        public CountryController(ICountryRepository countryRepository)
+        {
+            _countryRepository = countryRepository;
+        }
     }
 }
