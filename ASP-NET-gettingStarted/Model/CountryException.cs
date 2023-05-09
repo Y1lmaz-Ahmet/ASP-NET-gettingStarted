b50efed77f23644ad28e21d88092b19a1236bc1b
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-05-09 12:06:30 +0200

    aanmaken CountryException (custom exception)

--- ASP-NET-gettingStarted/Model/CountryException.cs
+++ ASP-NET-gettingStarted/Model/CountryException.cs
@@ -1,6 +1,23 @@
-﻿namespace ASP_NET_gettingStarted.Model
+﻿using System.Runtime.Serialization;
+
+namespace ASP_NET_gettingStarted.Model
 {
-    public class CountryException
+    public class CountryException : Exception
     {
+        public CountryException()
+        {
+        }
+
+        public CountryException(string? message) : base(message)
+        {
+        }
+
+        public CountryException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected CountryException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
     }
 }
