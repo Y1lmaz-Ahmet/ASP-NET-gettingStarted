766935ad9e1b3ba7df499407d385337c175de52e
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-05-09 10:59:08 +0200

    aanmaken controller

---
+++ ASP-NET-gettingStarted/Controllers/CountryController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_NET_gettingStarted.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+    }
+}
